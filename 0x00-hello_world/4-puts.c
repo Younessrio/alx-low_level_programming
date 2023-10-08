208a3ad49c5e26b6d26a05256f4bf77ece80089f
Younessrio <Youness.haidat1@gmail.com>
2023-10-08 13:29:45 -0700

    my work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,9 +11,9 @@
 
 int main(void)
 {
-    /* Use the puts function to print the string */
+/* Use the puts function to print the string */
     puts("\"Programming is like building a multilingual puzzle");
 
-    /* End the program with the value 0 (success) */
+/* End the program with the value 0 (success) */
     return (0);
 }	
