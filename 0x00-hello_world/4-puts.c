b47213748ef5f1c60f6d5b8fb6e8cdad0aca8bd7
Younessrio <Youness.haidat1@gmail.com>
2023-10-09 05:25:06 -0700

    my work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,35 @@
-#include <stdio.h>																																/**																	* main - Entry point of the program													*																	* Description: This function prints a specific string and returns 0,									* indicating successful program execution.												*																	* Return: Always 0 (success)														*/																																		int main(void)																{																		/* Use the puts function to print the string */												puts("\"Programming is like building a multilingual puzzle");																											/* End the program with the value 0 (success) */											return (0);															}																		
+#include <stdio.h>
+
+ 
+
+/**
+
+* main - Entry point of the program
+
+*
+
+* Description: This function prints a specific string and returns 0,
+
+* indicating successful program execution.
+
+*
+
+* Return: Always 0 (success)
+
+*/
+
+int main(void)
+
+{
+
+        /* Use the puts function to print the string */
+
+        puts("\"Programming is like building a multilingual puzzle");
+
+ 
+
+        /* End the program with the value 0 (success) */
+
+        return (0);
+
+}
